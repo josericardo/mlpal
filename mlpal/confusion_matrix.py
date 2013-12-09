83e3a66f9d7368e36250e6bad4eb95391bec8fdd
José Ricardo <zehzinho@gmail.com>
2013-12-09 20:43:26 -0200

    ConfusionMatrix#as_text_lines was crashing when no ...
    example was predicted with one of the labels

--- mlpal/confusion_matrix.py
+++ mlpal/confusion_matrix.py
@@ -7,17 +7,26 @@ class ConfusionMatrix:
     def predicted_as(self, claz):
         return (self.cm[0][claz] + self.cm[1][claz])
 
+    def _abs_and_per_of_false(self, i):
+        total = self.predicted_as(i)
+
+        if total == 0:
+            return (0, 0)
+
+        false_pred = self.cm[1-i][abs(0-i)]
+        perc = float(false_pred) / total
+        return (false_pred, (100*perc))
+
     def as_text_lines(self):
-        false_positives = self.cm[0][1]
-        false_negatives = self.cm[1][0]
-        per_fp = 100*(float(false_positives) / self.predicted_as(1))
-        per_fn = 100*(float(false_negatives) / self.predicted_as(0))
+        false_pos, per_fp = self._abs_and_per_of_false(1)
+        false_neg, per_fn = self._abs_and_per_of_false(0)
 
         result = [
-                "False positives: {} ({:.2f}% of all predicted as 1)".format(false_positives, per_fp),
-                "False negatives: {} ({:.2f}% of all predicted as 0)".format(false_negatives, per_fn)
+                "False positives: {} ({:.2f}% of all predicted as 1)".format(false_pos, per_fp),
+                "False negatives: {} ({:.2f}% of all predicted as 0)".format(false_neg, per_fn)
         ]
 
+        print result
         return result
 
     def as_str(self):
--- mlpal/tests/confusion_matrix_test.py
+++ mlpal/tests/confusion_matrix_test.py
@@ -12,3 +12,11 @@ class ConfusionMatrixTest(unittest.TestCase):
         str = ConfusionMatrix(raw_cm).as_str()
         self.assertTrue(str.find('False positives: 10 (50.00%') != -1)
         self.assertTrue(str.find('False negatives: 3 (25.00%') != -1)
+
+    def test_as_str_is_resilient_to_zero_labels_in_prediction(self):
+        raw_cm = [
+            [9, 0],
+            [3, 0]
+        ]
+
+        ConfusionMatrix(raw_cm).as_str()
