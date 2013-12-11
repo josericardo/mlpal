6d344d3cba3258e05ccf1acace540969b3b986fd
José Ricardo <zehzinho@gmail.com>
2013-12-11 11:27:49 -0200

    Generating Less points on the learning curves test.
    The goals is to see if it is working on more than one point.

--- mlpal/tests/run_targets_smoke_test.py
+++ mlpal/tests/run_targets_smoke_test.py
@@ -20,7 +20,7 @@ class RunTargetsSmokeTest(unittest.TestCase):
         self.assertEqual(None, exit_code_of(mlpal('plot_pca')), "plot_pca is failing")
 
     def test_learning_curves_is_ok(self):
-        self.assertEqual(None, exit_code_of(mlpal('learning_curves -n=100 --cv=2')), "learning_curves is failing")
+        self.assertEqual(None, exit_code_of(mlpal('learning_curves -n=100 --cv=2 --points=2')), "learning_curves is failing")
 
     def test_benchmark_is_ok(self):
         self.assertEqual(None, exit_code_of(mlpal('train')), "train is failing")
