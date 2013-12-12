390de8f3b78d737580aa1ccd273b47f33e81350b
José Ricardo <zehzinho@gmail.com>
2013-12-12 19:43:52 -0200

    Splitting BaseLearningSpecTest into two tests.

--- mlpal/base_learning_spec.py
+++ mlpal/base_learning_spec.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python
 
+
 class LearningSetupIsBroken(RuntimeError):
     pass
 
+
 class BaseLearningSpec:
-    """ Helper to make a LearningSpec definition easier 
+    """ Helper to make a LearningSpec definition easier
 
     There's only one method that must be overriden: #training_classifier
 
     Then, you can choose to define:
-    
+
     #gridsearch_pipelines: a custom GridSearch pipeline
 
     #gridsearch_params: define only the params for the #training_classifier
-                        and let the GridSearch pipeline be generated 
+                        and let the GridSearch pipeline be generated
                         automatically.
 
     *Warning* if you don't define any of the two above methods
@@ -23,15 +25,16 @@ class BaseLearningSpec:
         return (__file__, self.__class__.__name__)
 
     def training_classifier(self):
-        error = "%s#%s must define the #training_classifier method" % self._location()
-        raise LearningSetupIsBroken(error)
+        error = "%s#%s must define the #training_classifier method"
+        raise LearningSetupIsBroken(error % self._location())
 
     def gridsearch_params(self):
         return {'default': {}}
 
     def gridsearch_pipelines(self):
-        if 'default' not in  self.gridsearch_params():
-            error = "#gridsearch_params must return a hash with the 'default' key set"
+        if 'default' not in self.gridsearch_params():
+            error = ("#gridsearch_params must return a hash with the "
+                     "'default' key set")
             raise LearningSetupIsBroken(error)
 
         return {'default': {
