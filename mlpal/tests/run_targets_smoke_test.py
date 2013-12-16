6ff2a430bbaea6268417430b64e3b2c5c50b3a55
José Ricardo <zehzinho@gmail.com>
2013-12-16 15:38:54 -0200

    Making sure plot_pca works with and without pipelines

--- mlpal/tests/run_targets_smoke_test.py
+++ mlpal/tests/run_targets_smoke_test.py
@@ -12,9 +12,6 @@ class RunTargetsSmokeTest(unittest.TestCase):
     def test_search_is_ok(self):
         self.assertEqual(None, utils.run_mlpal('search --cv=3 -j 2 --random-state=42'), "search is failing")
 
-    def test_plot_pca_is_ok(self):
-        self.assertEqual(None, utils.run_mlpal('plot_pca'), "plot_pca is failing")
-
     def test_learning_curves_is_ok(self):
         self.assertEqual(None, utils.run_mlpal('learning_curves -n=100 --cv=2 --points=2'), "learning_curves is failing")
 
