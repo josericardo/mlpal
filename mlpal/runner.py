a79f9bcd5cb623f509bb1fcd21b98feb68588e08
José Ricardo <zehzinho@gmail.com>
2013-12-16 17:32:50 -0200

    Refactoring runner.py
    It had too many if's, it was getting ugly and it wasn't quite clear
    anymore.

--- mlpal/runner.py
+++ mlpal/runner.py
@@ -11,48 +11,59 @@ from benchmark import Benchmarker
 from misclassified import print_misclassified
 from learning_curves import plot_lcs
 from pca import plot_pca
+from history import History
 from importlib import import_module
 from config import Config
 
-def run_learning_curves(args, spec, data_source):
-    X, y = data_source.train_data()
-    plot_lcs(spec, X, y, args)
+def run_learning_curves(rt):
+    X, y = rt.data_source.train_data()
+    plot_lcs(rt.spec, X, y, rt.config)
 
-def run_benchmarks(args, data_source):
+def run_benchmark(rt):
+    args = rt.config
     print("Loading classifier: %s" % args.clfpath)
     classifier = joblib.load(args.clfpath)
     # check if the user has not defined one?
     args.log_to = 'benchmark.log'
-    benchmarker = Benchmarker(args, data_source, classifier)
+    benchmarker = Benchmarker(args, rt.data_source, classifier)
     benchmarker.run()
 
+def run_plot_pca(rt):
+    X, y = rt.data_source.train_data()
+
+    clf = rt.spec.training_classifier()
+    if isinstance(clf, Pipeline):
+        X = clf.fit_transform(X, y)
+
+    if X.shape[1] < 2:
+        raise RuntimeError('You must have at least 2 features to generate a PCA plot')
+
+    plot_pca(X, y)
+
+def run_train(rt):
+    trainer = Trainer(rt.config, rt.data_source,
+        rt.spec.training_classifier())
+    trainer.run()
+
+def run_search(rt):
+    searcher = Searcher(rt.config, rt.data_source)
+    searcher.fit(rt.spec.gridsearch_pipelines())
+
+def run_misclassified(rt):
+    print_misclassified(rt.config, rt.spec.training_classifier(), rt.data_source)
+
+class MLPalRuntime:
+    pass
+
 def run(args):
-    # TODO update args according to config.json here
     setup = import_module(args.setup)
+
+    runtime = MLPalRuntime()
+    runtime.info = History().new()
+    runtime.data_source = setup.DataSource(args)
+    runtime.spec = setup.LearningSpec()
+    runtime.config = args
+
     task = args.task
-    data_source = setup.DataSource(args)
-    spec = setup.LearningSpec()
-
-    if task == 'train':
-        trainer = Trainer(args, data_source, spec.training_classifier())
-        trainer.run()
-    elif task == 'search':
-        searcher = Searcher(args, data_source)
-        searcher.fit(spec.gridsearch_pipelines())
-    elif task == 'benchmark':
-        run_benchmarks(args, data_source)
-    elif task == 'learning_curves':
-        run_learning_curves(args, spec, data_source)
-    elif task == 'plot_pca':
-        X, y = data_source.train_data()
-
-        clf = spec.training_classifier()
-        if isinstance(clf, Pipeline):
-            X = clf.fit_transform(X, y)
-
-        if X.shape[1] < 2:
-            raise RuntimeError('You must have at least 2 features to generate a PCA plot')
-
-        plot_pca(X, y)
-    elif task == 'misclassified':
-        print_misclassified(args, spec.training_classifier(), data_source)
+    task_function = getattr(sys.modules[__name__], 'run_%s' % task)
+    task_function(runtime)
